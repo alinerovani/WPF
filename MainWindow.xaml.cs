fd003979a367804d0e6b0a45a5eb286ed0908eac
Aline Rovani <lopesalini@gmail.com>
2022-08-02 16:30:33 +0200

    added edit and delete options

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Names
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            btnDelete.Visibility = Visibility.Hidden;
+            btnUpdate.Visibility = Visibility.Hidden;
+        }
+
+        private enum Operation
+        {
+            Update = 1,
+            Delete = 2
+        }
+
+        private void ValidateName()
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+                throw new Exception("Name isn't defined!");
+
+            if (lstNames.Items.Contains(txtName.Text))
+                throw new Exception("Name cannot be duplicated!");
+        }
+
+        private void AddName()
+        {
+            try
+            {
+                ValidateName();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return;
+            }
+
+            lstNames.Items.Add(txtName.Text);
+            txtName.Clear();
+        }
+
+        private void ChangeName(Operation operation)
+        {
+            switch (operation)
+            {
+                case Operation.Update:
+                    try
+                    {
+                        if (!lstNames.Items[lstNames.SelectedIndex].Equals(txtName.Text))
+                            ValidateName();
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.Message);
+                        return;
+                    }
+
+                    lstNames.Items[lstNames.SelectedIndex] = txtName.Text;
+                    break;
+                case Operation.Delete:
+                    lstNames.Items.RemoveAt(lstNames.SelectedIndex);
+                    break;
+            }
+
+            txtName.Clear();
+
+            btnDelete.Visibility = Visibility.Hidden;
+            btnUpdate.Visibility = Visibility.Hidden;
+            btnAdd.Visibility = Visibility.Visible;
+        }
+
+        private void lstNames_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lstNames.SelectedItem == null)
+                return;
+
+            txtName.Text = lstNames.SelectedItem.ToString();
+
+            btnAdd.Visibility = Visibility.Hidden;
+            btnDelete.Visibility = Visibility.Visible;
+            btnUpdate.Visibility = Visibility.Visible;
+
         }
 
         protected void btnAdd_Click(object sender, RoutedEventArgs e)
@@ -30,22 +92,14 @@ namespace Names
             AddName();
         }
 
-        private void AddName()
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(txtName.Text))
-            {
-                MessageBox.Show("Name isn't defined!");
-                return;
-            }
+            ChangeName(Operation.Update);
+        }
 
-            if (lstNames.Items.Contains(txtName.Text))
-            {
-                MessageBox.Show("Name cannot be duplicated!");
-                return;
-            }
-
-            lstNames.Items.Add(txtName.Text);
-            txtName.Clear();
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeName(Operation.Delete);
         }
     }
 }
